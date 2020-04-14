dc33c384cc23c9ceeab1446896cc3124281edab3
dn-ra <daniel.rawlinson@unimelb.edu.au>
2020-04-14 16:27:12 +1000

    output bams

--- scripts/make_bams.sh
+++ scripts/make_bams.sh
@@ -2,7 +2,7 @@
 READS=''
 
 #map to whole genome
-minimap2 -ax splice -un ../data/SARS-Cov2/wuhan_coronavirus_australia.fasta $READS | samtools view -hF4b > whole_genome_mapped.bam
+minimap2 -ax splice -un ../data/SARS-Cov2/wuhan_coronavirus_australia.fasta $READS | samtools view -hF4 -b > whole_genome_mapped.bam
 
 #map to leaderr
-minimap2 -ax map-ont -un ../data/SARS-Cov2/leader.fasta $READS | satmools view -hF4b > leader_mapped.bam
+minimap2 -ax map-ont -un ../data/SARS-Cov2/leader.fasta $READS | satmools view -hF4 -b > leader_mapped.bam
