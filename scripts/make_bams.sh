2ec44864051e0653f7ef72aebb6a3218f1d3aa46
dn-ra <daniel.rawlinson@unimelb.edu.au>
2020-04-14 11:48:57 +1000

    Initial commit

---
+++ scripts/make_bams.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+READS=''
+
+#map to whole genome
+minimap2 -ax splice -un ../data/SARS-Cov2/wuhan_coronavirus_australia.fasta $READS | samtools view -hF4b > whole_genome_mapped.bam
+
+#map to leaderr
+minimap2 -ax map-ont -un ../data/SARS-Cov2/leader.fasta $READS | satmools view -hF4b > leader_mapped.bam
