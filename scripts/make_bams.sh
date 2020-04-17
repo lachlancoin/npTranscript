2bde8c2029d718319c38b6c29cb57f2620e79437
dn-ra <daniel@login.novalocal>
2020-04-17 13:36:29 +1000

    sort by default, input multiple fastqs

--- scripts/make_bams.sh
+++ scripts/make_bams.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -e
-READS=''
+
+declare -a READS
+for read_file in ${@}; do READS+=($(readlink -f $read_file)); done
+
+echo $READS
+
+MM2='/sw/minimap2/minimap2-2.11_x64-linux/minimap2'
+ORIG_DIR=$(pwd)
+
+cd $(dirname $0)
+
 
 #map to whole genome
-minimap2 -ax splice -un ../data/SARS-Cov2/wuhan_coronavirus_australia.fasta $READS | samtools view -hF4 -b > whole_genome_mapped.bam
+$MM2 -ax splice -un ../data/SARS-Cov2/wuhan_coronavirus_australia.fasta.gz $READS | samtools view -hF4 -b | samtools sort - > $ORIG_DIR/sorted.whole_genome_mapped.bam
 
 #map to leaderr
-minimap2 -ax map-ont -un ../data/SARS-Cov2/leader.fasta $READS | satmools view -hF4 -b > leader_mapped.bam
+$MM2 -ax map-ont -un ../data/SARS-Cov2/leader.wuhan_coronavirus_australia.fasta.gz $READS | samtools view -hF4 -b | samtools sort - > $ORIG_DIR/sorted.leader_mapped.bam
+
+
+cd $ORIG_DIR
