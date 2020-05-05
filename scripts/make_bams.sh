24fa382e5608e3cbaf93ee25f390fd4c9addc755
dn-ra <daniel.rawlinson@unimelb.edu.au>
2020-05-05 14:26:17 +1000

    remove leader, host with splice

--- scripts/make_bams.sh
+++ scripts/make_bams.sh
@@ -4,11 +4,11 @@ declare -a READS=${@}
 echo "Mapping reads from: ${READS[@]}"
 MM2='/sw/minimap2/minimap2-2.11_x64-linux/minimap2'
 npT=$(dirname $0)
+HOST=''
+
 
 #map to whole genome
 $MM2 -ax splice -un $npT/../data/SARS-Cov2/wuhan_coronavirus_australia.fasta.gz $READS | samtools view -hF4 -b | samtools sort - > whole_genome_mapped.bam
-samtools index whole_genome_map.bam
 
-#map to leaderr
-#$MM2 -ax map-ont -un $SCRIPT_DIR/../data/SARS-Cov2/leader.wuhan_coronavirus_australia.fasta.gz $READS | samtools view -hF4 -b | samtools sort - > sorted.leader_mapped.bam
-
+#map to host
+$MM@ -ax splice -uf $HOST $READS | samtools view -hF4 -b | samtools sort - > host_mapped.bam
