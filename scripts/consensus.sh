aa4b93122a7095835788d4c03b8f73d7def03605
Lachlan Coin <ljmcoin@gmail.com>
2020-05-13 16:30:37 +1000

    update to scripts

--- scripts/consensus.sh
+++ scripts/consensus.sh
@@ -18,33 +18,36 @@ abpoa='/sw/abpoa/v1.0.1/abpoa'
 npTranscript=${HOME}/github/npTranscript
 reference="${npTranscript}/data/SARS-Cov2/wuhan_coronavirus_australia.fasta.gz"
 
-chrom=$SLURM_ARRAY_TASK_ID
-if [ ! $SLURM_ARRAY_TASK_ID ] ; then
-chrom=$1
-fi
+chrom=0
+n=$SLURM_ARRAY_TASK_ID
+#chrom=$SLURM_ARRAY_TASK_ID
+#if [ ! $SLURM_ARRAY_TASK_ID ] ; then
+#chrom=$1
+#fi
 
 
 zipfile="${chrom}.clusters.zip"
 tododir="${chrom}.clusters"
 mkdir -p $tododir
-todolist=$chrom.todo.txt
-unzip -l $zipfile  | tr -s ' ' | cut -f 5 -d ' ' | grep 'ID' | grep '.fa' > $todolist
-outfile="${chrom}.consensus.fasta"
+todolist=$chrom.$n.todo.txt
+unzip -l $zipfile  | tr -s ' ' | cut -f 5 -d ' ' | grep -v 'ID' | grep '.fa' | tail -n +$n | head -n 1 > $todolist
+outfile="${chrom}.$n.consensus_orf.fasta"
 rm $outfile
 while read entry; do
 	echo $entry
 	#entry=$(head -n $n $todolist | tail -n +1)
 	entryname=$(echo $entry | sed 's/.fa//')
 	clustid=$(echo $entry | cut -f 1-2 -d .)
-	desc=$(less $chrom.transcripts.txt.gz | grep -P "${clustid}\t" | head -n 1 | sed 's/\t/;/g' | cut -f 1-12 -d ';')
+	#desc=$(less $chrom.transcripts.txt.gz | grep -P "${clustid}\t" | head -n 1 | sed 's/\t/;/g' | cut -f 1-12 -d ';')
 	tmpfile="${tododir}/${entry}"
 	#outfile="${tododir}/${entry}.out"
 	unzip -p $zipfile $entry > $tmpfile
-	${abpoa} $tmpfile | sed "s/Consensus_sequence/${entryname} ${desc}/" >> $outfile
+	${abpoa} $tmpfile | sed "s/Consensus_sequence/${entryname}/" >> $outfile
+	rm -f $tmpfile
 done < $todolist
 rm $todolist
-rm -f $tododir/*.fa
-rmdir $tododir
+
+#rmdir $tododir
 
 
 ##NOW MAP TO REF
