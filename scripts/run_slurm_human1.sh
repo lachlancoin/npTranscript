2f23b711c72e4a344b5a6abc109f97c0d978f0b7
Lachlan Coin <ljmcoin@gmail.com>
2020-08-14 15:57:41 +1000

    update

--- scripts/run_slurm_human1.sh
+++ scripts/run_slurm_human1.sh
@@ -15,42 +15,33 @@
 ##tip - use symbolic link to put this in the directory with bam files
 export JSA_MEM=30000m
 npTranscript=${HOME}/github/npTranscript
+dat=$(date +%Y%m%d%H%M%S)
 
-#bamfiles=bins/allreads_fq.bam:virion/sorted.virion_refmap.bam
 bamdir="."
-bamc=$(ls  | grep '.bam' | wc  -l | cut -f 1)
-if [ $bamc -gt 0 ]; then 
-	bamfiles=$(ls ${bamdir} | grep '.bam$' | xargs -I {} echo ${bamdir}/{})
-	bamfiles_="--bamFile=${bamfiles}"
-else
-        bamfiles=$(ls ${bamdir} | grep '.fastq$' | xargs -I {} echo ${bamdir}/{})
-        bamfiles_="--fastqFile=${bamfiles}"
-fi
+bamfiles=$(ls ${bamdir} | grep '.bam$' | xargs -I {} echo ${bamdir}/{})
+bamfiles_="--bamFile=${bamfiles}"
 bamfiles1=$(echo $bamfiles_ | sed 's/ /:/g')
-echo $bamfiles1
-opts1="" 
 
-#following if you want to only analysis a subset of reads
-a=$(ls  | grep '^reads_in.txt$'  | wc -l)
-if [ $a -eq 1 ]; then 
-  opts1="--readList reads_in.txt" 
-fi
-
+##SPECIFY LOCATION OF COMBINED AND VIRUS ONLY DB
 #reference="../Chlorocebus_sabaeus.ChlSab1.1.dna.toplevel.fa.gz"
 #coord_file="../Chlorocebus_sabaeus.ChlSab1.1.99.gff3.gz"
 reference="./human_virus_sequin_ensembl_pri_merged_genome.fasta"
 coord_file="./gencode.v28.annotation.gff3.gz"
-dat=$(date +%Y%m%d%H%M%S)
+reference_virus="${npTranscript}/data/SARS-Cov2/VIC01/wuhan_coronavirus_australia.fasta.gz"
+coord_file_virus="${npTranscript}/data/SARS-Cov2/VIC01/Coordinates.csv"
+cov_chr=$(zcat ${coord_file_virus} | head -n 1 | cut -f 1 -d ' ' | sed 's/>//g')
+echo "coronavirus chr id ${cov_chr}" 
 resdir="results_${dat}"
 opts="--bin=100 --breakThresh=100 --coronavirus=false --maxThreads=8 --extra_threshold=1000 --writePolyA=true --msaDepthThresh=1000 --doMSA=false --numExonsMSA=1:2:3:4:5 --msa_source=RNA --useExons=true --span=protein_coding --includeStart=false --isoformDepthThresh 1000"
+
 #for dRNA datasets
 opts="${opts} --RNA=true"
-opts2="--fail_thresh=0  --chromsToRemap=MT007544.1  --mm2_memory=10g --recordDepthByPosition=true"
-opts3="--GFF_features=ID:description:ID:gene_type:gene_name"
+opts2="--fail_thresh=0  --chromsToRemap=${cov_chr}  --mm2_memory=10g --recordDepthByPosition=true"
+opts3="--GFF_features=gene_id:description:gene_name:gene_type:gene_name"
 
-#opts="${opts} --maxReads 10000"
 bash ${npTranscript}/scripts/run.sh ${bamfiles1}   --reference=${reference} --annotation ${coord_file} --resdir ${resdir} ${opts} ${opts1} ${opts2} ${opts3}
 
 
+
 cd ${resdir}
-#Rscript ~/github/npTranscript/R/npDE.R  control infected betabinom
+Rscript ${npTranscript}/R/npDE.R  control infected betabinom
