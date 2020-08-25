3d67e7fac4b53d65f7d9d7156ed8677f86eb983f
Lachlan Coin <ljmcoin@gmail.com>
2020-08-25 23:12:38 +1000

    update to slurm script

--- scripts/run_slurm.sh
+++ scripts/run_slurm.sh
@@ -19,12 +19,8 @@ npTranscript=${HOME}/github/npTranscript
 #bamfiles=bins/allreads_fq.bam:virion/sorted.virion_refmap.bam
 bamdir="."
 bamfiles=$(ls ${bamdir} | grep '.bam$' | xargs -I {} echo ${bamdir}/{})
-bamfiles="--bamFile=${bamfiles}" 
-if [ ! $bamfiles ]; then 
-	bamfiles=$(ls ${bamdir} | grep '.fastq$' | xargs -I {} echo ${bamdir}/{})
-	bamfiles="--fastqFile=${bamfiles}" 
-fi
-bamfiles1=$(echo $bamfiles | sed 's/ /:/g')
+bamfiles_="--bamFile=${bamfiles}"
+bamfiles1=$(echo $bamfiles_ | sed 's/ /:/g')
 echo $bamfiles1
 opts1="" 
 a=$(ls  | grep '^reads_in.txt$'  | wc -l)
@@ -32,22 +28,22 @@ if [ $a -eq 1 ]; then
   opts1="--readList reads_in.txt" 
 fi
 
-reference="${npTranscript}/data/SARS-Cov2/NC/NC_045512.fasta.gz" 
-coord_file="${npTranscript}/data/SARS-Cov2/NC/Coordinates.csv"
+#reference="${npTranscript}/data/SARS-Cov2/NC/NC_045512.fasta.gz" 
+#coord_file="${npTranscript}/data/SARS-Cov2/NC/Coordinates.csv"
 
-#reference="${npTranscript}/data/SARS-Cov2/VIC01/wuhan_coronavirus_australia.fasta.gz"
-#coord_file="${npTranscript}/data/SARS-Cov2/VIC01/Coordinates.csv"
+reference="${npTranscript}/data/SARS-Cov2/VIC01/wuhan_coronavirus_australia.fasta.gz"
+coord_file="${npTranscript}/data/SARS-Cov2/VIC01/Coordinates.csv"
 #alias abpoa='/sw/abpoa/v1.0.1/abpoa'
 dat=$(date +%Y%m%d%H%M%S)
 echo $dat
 resdir="results_${dat}"
 ##note that to do separate msa for multiple input bams you type --doMsa=5_3:sep
-opts="--bin=100 --breakThresh=1000  --isoformDepthThresh=10000 --coverageDepthThresh=0 --extra_threshold=200 --msaDepthThresh=20 --doMSA=all:sep --reAlignExtra=true --bedChr=NC_045512v2"
-opts2="--fail_thresh=7" 
+opts="--bin=100 --RNA true --breakThresh=1000  --isoformDepthThresh=10000 --coverageDepthThresh=0 --extra_threshold=200 --msaDepthThresh=20 --doMSA=all:sep --reAlignExtra=true --bedChr=NC_045512v2"
+opts2="--fail_thresh=7 --recordDepthByPosition=true" 
 #opts="${opts} --maxReads 10000"
 bash ${npTranscript}/scripts/run.sh ${bamfiles1}   --reference=${reference} --annotation ${coord_file} --resdir ${resdir} ${opts} ${opts1} ${opts2}
 
-cd ${resdir}
+#cd ${resdir}
 #bash ${npTranscript}/scripts/consensus.sh 0
-echo "now running R script"
+#echo "now running R script"
 #Rscript ~/github/npTranscript/R/npTranscript.R  ~/github/npTranscript/data/SARS-Cov2/VIC01
