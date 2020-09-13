32877e6f6e085b3ef000369834da007705a541ab
dn-ra <daniel.rawlinson@unimelb.edu.au>
2020-09-13 15:35:34 +1000

    default unzip, default consensus top 200 transcripts

--- scripts/run_consensus.sh
+++ scripts/run_consensus.sh
@@ -11,19 +11,32 @@ if [ ! $1 ]; then
  fa="clust"
 fi
 
+max_clust=$3
+if [ ! $3 ]; then
+  max_clust=200
+fi
+
+unzip=true
 
 todo=$(ls *.clusters.zip | grep $fa | xargs -I {} echo {} | rev | cut -f 3- -d . | rev)
 for i in $todo; do
 	echo $i.clusters.zip
 	echo $i.clusters
-	mkdir $i.clusters
-	unzip -n $i.clusters.zip -d $i.clusters
+	if [ "$unzip" == true ]; then
+		mkdir $i.clusters
+		unzip -n $i.clusters.zip -d $i.clusters
+	fi;
+
 	cd $i.clusters
 	for j in *.fa; do
 		a=$(grep '>' $j | wc -l)
-		if [ $a -lt $thresh ] ;then rm $j ; fi;
-	done
-	ls | grep '.fa' > ../$i.todo.txt
+		if [ $a -lt $thresh ] ;then rm $j  
+		else echo ${j} ${a}
+	fi;
+	done | sort -rn -k2 | head -n ${max_clust} | cut -d " " -f 1 > ../$i.todo.txt
+
+
+	#ls | grep '.fa' > ../$i.todo.txt
         cd ../
 	#less $i.clusters.zip | grep $fa > $i.todo.txt
 	cnt=$(wc -l $i.todo.txt | cut -f 1 -d ' ')
