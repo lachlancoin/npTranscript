bb308a58b3468cd8908413b054b8f910d3fce11d
Lachlan Coin <ljmcoin@gmail.com>
2020-09-15 18:45:53 +1000

    update

--- scripts/run_filter.sh
+++ scripts/run_filter.sh
@@ -8,8 +8,9 @@ fi
 npTranscript=$HOME/github/npTranscript
 classp=$(ls ${npTranscript}/libs | xargs -I {} echo ${npTranscript}/libs/{} )
 classpath=$(echo $classp | sed 's/ /:/g')
-JSA_CP=${npTranscript}/npTranscript.jar:${classpath}
+#JSA_CP=${npTranscript}/npTranscript.jar:${classpath}
 #echo $JSA_CP
+JSA_CP=${npTranscript}/java/target/npTranscript-1.0.jar 
 
 
 
