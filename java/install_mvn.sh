3f584fa08e07e3e6803e1d14d4b49c67a466009a
Lachlan Coin <lcoin@spartan-login2.hpc.unimelb.edu.au>
2021-06-13 18:00:18 +1000

    update

--- java/install_mvn.sh
+++ java/install_mvn.sh
@@ -1,4 +1,9 @@
-mvn install:install-file -Dfile=./libs/japsa-1.9.4e.jar  -DgroupId=coin -DartifactId=japsa         -Dversion=1.9.4e        -Dpackaging=jar
+#!/bin/bash
+
+
+module load java
+module load maven/3.6.3
+mvn install:install-file -Dfile=./libs/japsacov-1.9.5e.jar -DgroupId=coin -DartifactId=japsacov         -Dversion=1.9.5e        -Dpackaging=jar
 mvn install:install-file -Dfile=./libs/sis-base-18.09.0.jar  -DgroupId=ch.ethz.wiki-bsse -DartifactId=sis-base     -Dversion=18.09.0      -Dpackaging=jar
 mvn install:install-file -Dfile=./libs/sis-jhdf5-19.04.0.jar  -DgroupId=ch.ethz.wiki-bsse -DartifactId=sis-jhdf5         -Dversion=19.04.0      -Dpackaging=jar
 
