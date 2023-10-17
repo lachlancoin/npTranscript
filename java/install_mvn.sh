d94312bed1217e86141f28db8a8078a2f53d5301
Lachlan Coin <ljmcoin@example.com>
2023-10-18 10:40:29 +1100

    update

--- java/install_mvn.sh
+++ java/install_mvn.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+module load Java
+#module load java/1.8.0_241
+#module load java
+module load Maven/3.9.3
+#module load maven/3.6.3
+reinstall=$1
+#module load anaconda2/2019.10
+#conda activate build
+git pull
 
-
-module load java
-module load maven/3.6.3
-mvn install:install-file -Dfile=./libs/japsacov-1.9.5e.jar -DgroupId=coin -DartifactId=japsacov         -Dversion=1.9.5e        -Dpackaging=jar
-mvn install:install-file -Dfile=./libs/sis-base-18.09.0.jar  -DgroupId=ch.ethz.wiki-bsse -DartifactId=sis-base     -Dversion=18.09.0      -Dpackaging=jar
-mvn install:install-file -Dfile=./libs/sis-jhdf5-19.04.0.jar  -DgroupId=ch.ethz.wiki-bsse -DartifactId=sis-jhdf5         -Dversion=19.04.0      -Dpackaging=jar
-
+if [ $reinstall ]; then
+ mvn install:install-file -Dfile=./libs/japsacov-1.9.5e.jar -DgroupId=coin -DartifactId=japsacov         -Dversion=1.9.5e        -Dpackaging=jar
+ mvn install:install-file -Dfile=./libs/sis-base-18.09.0.jar  -DgroupId=ch.ethz.wiki-bsse -DartifactId=sis-base     -Dversion=18.09.0      -Dpackaging=jar
+ mvn install:install-file -Dfile=./libs/sis-jhdf5-19.04.0.jar  -DgroupId=ch.ethz.wiki-bsse -DartifactId=sis-jhdf5         -Dversion=19.04.0      -Dpackaging=jar
+fi
 
 jardir=~/github/JNAerator/jnaerator/target
-java -jar ${jardir}/jnaerator-0.13-SNAPSHOT-shaded.jar -mode Jar -runtime JNA ~/github/edlib/edlib/include/edlib.h ~/github/edlib/meson-build/libedlib.so
-mvn install:install-file -Dfile=out.jar  -DgroupId=edlib -DartifactId=edlib         -Dversion=1.2.7      -Dpackaging=jar
-
+#rm out.jar
+if [ ! -s out.jar ] ; then
+ java -jar ${jardir}/jnaerator-0.13-SNAPSHOT-shaded.jar -mode Jar -runtime JNA ~/github/edlib/edlib/include/edlib.h ~/github/edlib/meson-build/libedlib.so
+ mvn install:install-file -Dfile=out.jar  -DgroupId=edlib -DartifactId=edlib         -Dversion=1.2.7      -Dpackaging=jar
+fi
 
 mvn clean package install
 
@@ -19,4 +28,10 @@ jar xf out.jar lib/linux_x64/libedlib.so
 mkdir target/classes/linux-x86-64
 mv lib/linux_x64/libedlib.so target/classes/linux-x86-64
 rm -rf lib
-rm out.jar
+#rm out.jar
+#module load gcc
+module load GCC/11.3.0
+
+export mainclass=npTranscript.run.Barcodes
+bash ../scripts/run.sh
+export mainclass=npTranscript.run.ViralTranscriptAnalysisCmd2
